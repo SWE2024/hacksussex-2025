f8a3ba55da4853d4ee1bf29d1947d4c930545757
Koushic <koushic.sk@yahoo.com>
2025-02-22 14:05:15 +0000

    Implemented CORS in backend

--- backend/main.py
+++ backend/main.py
@@ -2,9 +2,21 @@ from typing import Union
 
 from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+origins = [
+    "http://localhost:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 university_file = open('./lists/universities.txt', 'r')
 university_list = university_file.readlines()
