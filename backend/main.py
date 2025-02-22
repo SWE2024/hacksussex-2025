1f1d77bafa234cb78b1253218a16be8d51cebaa2
Lewis <lewisryework@gmail.com>
2025-02-22 13:48:29 +0000

    Add create_register

--- backend/main.py
+++ backend/main.py
@@ -1,15 +1,30 @@
 from typing import Union
 
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
 
 app = FastAPI()
 
 
+university_file = open('./lists/universities.txt', 'r')
+university_list = university_file.readlines()
+university_list = [line.strip() for line in university_list] # remove the whitespace and newlines
+
+type_file = open('./lists/types.txt', 'r')
+type_list = type_file.readlines()
+type_list = [line.strip() for line in type_list] # remove the whitespace and newlines
+
+
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def root():
+    raise HTTPException(status_code=400, detail="Endpoint does not exist")
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+@app.get("/signup")
+def create_register():
+    return { "universities":university_list, "types":type_list }
+
+
+@app.post("/signup")
+def register():
+    raise HTTPException(status_code=201, detail="Not implemented")
