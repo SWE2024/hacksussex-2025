1cb621ceba8d8defd709d9e8ef8fe349544ce5ff
Axel <axelgumiit2@gmail.com>
2025-02-22 13:34:59 +0000

    added a LogIn page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route, Outlet } from "react-router-dom
 import { CssBaseline } from "@mui/material";
 import SignUp from "./pages/SignUp";
 import PageNotFound from "./pages/PageNotFound";
+import LogIn from "./pages/LogIn";
 
 function Layout() {
 
@@ -18,10 +19,12 @@ function App() {
     <Routes>
 
       <Route path="/signup" element={<SignUp />} />
+      <Route path="/login" element={<LogIn />} />
 
       {/* Protected Routes */}
       <Route element={<Layout />}>
         {/*<Route path="/dashboard" element={isAuthenticated ? <Dashboard /> : <Login />} />*/}
+
       </Route>
 
       {/* Catch-all route for undefined paths */}
