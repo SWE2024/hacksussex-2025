b1b799e00e24fb0b60ddb558c05199feb13bffd0
Lewis <lewisryework@gmail.com>
2025-02-23 03:52:29 +0000

    Bugfix create module

--- backend/routes/module.py
+++ backend/routes/module.py
@@ -14,16 +14,18 @@ router = APIRouter(prefix="", tags=["Users"])
 def create_module(request: Request, session: database.SessionDeP, module_name: str = Form(...), credits: int = Form(...), year: str = Form(...), email: str = Form(...)):
     statement = select(User).where(User.email == email)
     user = session.exec(statement).first()
-    statement_year = select(Year).where((Year.num == year) & (Year.user_id == user.id))
-    year_ = session.exec(statement_year).first()
 
     if user == None:
         raise HTTPException(status_code=401, detail="User does not exist")
+
+    statement_year = select(Year).where((Year.num == year) & (Year.user_id == user.id))
+    year_ = session.exec(statement_year).first()
+
     if year_ == None:
         print("year not found for this user")
         raise HTTPException(status_code=404, detail="Year not found for this user")
 
-    new_module = Module(module_name=module_name, credits=credits, year_id=year_.id, user_id=user.id)
+    new_module = Module(name=module_name, credits=credits, year_id=year_.id)
     session.add(new_module)
     session.commit()
 
